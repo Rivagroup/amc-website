4768c674450e68a2e946fcec213d5385b2c5771f
Oliver Oehme <oliver.oehme@gmail.com>
2020-10-27 08:43:42 +0100

    wrong number

--- application/views/buchung/buchung_offline.php
+++ application/views/buchung/buchung_offline.php
@@ -2,6 +2,6 @@
   <h1 class="display-4"><i class="fas fa-exclamation-triangle"></i> System Offline</h1>
   <p class="lead">Leider ist unser Buchungssystem gerade offline. Du kannst aber gern telefonisch unter <b class="text-nowrap">0371 334 2 334-0</b> buchen.</p>
   <p class="lead">
-    <a class="btn btn-info btn-lg btn-block" href="tel:+491733071490" role="button"><i class="fas fa-phone"></i> telefonisch buchen</a>
+    <a class="btn btn-info btn-lg btn-block" href="tel:+4937133423340" role="button"><i class="fas fa-phone"></i> telefonisch buchen</a>
   </p>
 </div>
