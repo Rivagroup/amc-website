122d1ef52ea9de8dc56a032b9a634c99c569f267
Oliver Oehme <oliver.oehme@gmail.com>
2020-10-27 16:56:04 +0100

    typos

--- application/views/kindergeburtstag/overview.php
+++ application/views/kindergeburtstag/overview.php
@@ -18,8 +18,8 @@
             <div class="card-footer">
                 <p class="display-3 text-center"><b>45 €</b></p>
                 <a class="btn btn-sm btn-block btn-info" href="<?= base_url('kindergeburtstag/basic'); ?>"><i class="fas fa-info"></i> weitere Informationen</a>
-                <a class="btn btn-lg btn-block btn-primary" href="tel:+4937133423340"><i class="fas fa-phone"></i> telefonisch Buchen</a>
-                <a class="btn btn-lg btn-block btn-primary disabled" href="<?= base_url('buchung/kindergeburtstagBasic'); ?>"><i class="far fa-calendar-plus"></i> online Buchen</a>
+                <a class="btn btn-lg btn-block btn-primary" href="tel:+4937133423340"><i class="fas fa-phone"></i> telefonisch buchen</a>
+                <a class="btn btn-lg btn-block btn-primary disabled" href="<?= base_url('buchung/kindergeburtstagBasic'); ?>"><i class="far fa-calendar-plus"></i> online buchen</a>
             </div>
         </div>
         <div class="card bg-warning text-dark">
@@ -39,8 +39,8 @@
             <div class="card-footer">
                 <p class="display-3 text-center"><b>70 €</b></p>
                 <a class="btn btn-sm btn-block btn-warning" href="<?= base_url('kindergeburtstag/maxi'); ?>"><i class="fas fa-info"></i> weitere Informationen</a>
-                <a class="btn btn-lg btn-block btn-primary" href="tel:+4937133423340"><i class="fas fa-phone"></i> telefonisch Buchen</a>
-                <a class="btn btn-lg btn-block btn-primary disabled" href="<?= base_url('buchung/kindergeburtstagMaxi'); ?>"><i class="far fa-calendar-plus"></i> online Buchen</a>
+                <a class="btn btn-lg btn-block btn-primary" href="tel:+4937133423340"><i class="fas fa-phone"></i> telefonisch buchen</a>
+                <a class="btn btn-lg btn-block btn-primary disabled" href="<?= base_url('buchung/kindergeburtstagMaxi'); ?>"><i class="far fa-calendar-plus"></i> online buchen</a>
             </div>
         </div>
         <div class="card bg-success text-white">
@@ -62,8 +62,8 @@
             <div class="card-footer">
                 <p class="display-3 text-center"><b>85 €</b></p>
                 <a class="btn btn-sm btn-block btn-success" href="<?= base_url('kindergeburtstag/xxl'); ?>"><i class="fas fa-info"></i> weitere Informationen</a>
-                <a class="btn btn-lg btn-block btn-primary" href="tel:+4937133423340"><i class="fas fa-phone"></i> telefonisch Buchen</a>
-                <a class="btn btn-lg btn-block btn-primary disabled" href="<?= base_url('buchung/kindergeburtstagXXL'); ?>"><i class="far fa-calendar-plus"></i> online Buchen</a>
+                <a class="btn btn-lg btn-block btn-primary" href="tel:+4937133423340"><i class="fas fa-phone"></i> telefonisch buchen</a>
+                <a class="btn btn-lg btn-block btn-primary disabled" href="<?= base_url('buchung/kindergeburtstagXXL'); ?>"><i class="far fa-calendar-plus"></i> online buchen</a>
             </div>
         </div>
     </div>
