8fc1fa52cea66fc5b962c84e1f3237feb7b31f33
Oliver Oehme <oliver.oehme@gmail.com>
2020-10-27 10:21:58 +0100

    cleaned up booking controller

--- application/controllers/Buchung.php
+++ application/controllers/Buchung.php
@@ -35,33 +35,4 @@ class Buchung extends MY_ContentController {
         $data['main_content'] = 'buchung/buchung_offline';
         $this->load->view('template', $data);
     }
-
-    public function einzelspiel(){
-		$data=$this->meta4all($this->meta);
-        $data['bookingurl'] = "https://10669.pc-booking.com/apps/v3/#?t=1589738049403&grp=649f4273-c7fc-4923-aaa1-bbeb4bbf9995&category=1&subcategory=10";
-        $data['title'] .= ' - EinzelSpiele';
-        $data['main_content'] = 'buchung/buchung_content';
-        //$this->load->view('template', $data);
-        redirect('/buchung/offline');
-    }
-
-    public function fuenferticket(){
-		$data=$this->meta4all($this->meta);
-        $data['bookingurl'] = "https://10669.pc-booking.com/apps/v3/#?t=1589739925898&grp=649f4273-c7fc-4923-aaa1-bbeb4bbf9995&category=1&subcategory=9";
-        $data['title'] .= ' - 5er Ticket';
-        $data['main_content'] = 'buchung/buchung_content';
-        //$this->load->view('template', $data);
-        redirect('/buchung/offline');
-    }
-
-    public function allyoucantag(){
-		$data=$this->meta4all($this->meta);
-        $data['bookingurl'] = "";
-        $data['title'] = ' - All You Can Tag';
-        $data['main_content'] = 'buchung/buchung_content';
-        //$this->load->view('template', $data);
-        redirect('/buchung/offline');
-    }
-
-
 }
