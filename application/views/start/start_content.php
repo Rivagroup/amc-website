222b4df9b377071fda2094fc3bc2aae89eca51d9
Oliver Oehme <oliver.oehme@gmail.com>
2020-10-29 11:44:18 +0100

    china virus update

--- application/views/start/start_content.php
+++ application/views/start/start_content.php
@@ -3,6 +3,50 @@
 <p>
 Ihr bewegt euch spielerisch auf einem 16-Loch-Indoor-Minigolf-Parcours durch 4 unterschiedliche Fantasiewelten. Die gesamte Spielfläche wurde mit Wandmotiven in fluoreszierenden Farben gestaltet, die im Schwarzlicht gut zur Geltung kommen. Der Freizeitspaß ist natürlich wetter unabhängig und eignet sich daher sehr gut ganzjährig für gemeinsame Aktivitäten mit Freunden und Familie, für Junggesellenabschiede, Geburtstag oder auch Firmenevents.
 </p>
+
+<div class="card shadow-sm text-white bg-danger my-3" style="width: auto;">
+    <img src="<?= base_url('assets/header/Corona.jpg'); ?>" class="card-img-top" alt="China-Virus">
+    <div class="card-body">
+    <h4 class="card-title text-uppercase"><i class="fas fa-viruses"></i> COVID-19 INFORMATIONEN</h4>
+        <div class="card-text text-justify text-white">           
+            <h5>Aktuelle Statistik zum Infektions-Geschehen im Spielcenter-Chemnitz</h5>
+            <p class="small">seit Erklärung zur Pandemie am 11. März 2020 | Stand: 26.10.2020</p>
+            <div class="table-responsive">
+                <table class="table table-striped text-white table-borderless">
+                    <tbody>
+                    <tr>
+                        <td>bestätigte Fälle:</td>
+                        <td><b>0</b></td>
+                    </tr>
+                    <tr>
+                        <td>bestätigte Neuinfektionen:</td>
+                        <td><b>0</b></td>
+                    </tr>
+                    <tr>
+                        <td>davon Intensiv-Patienten:</td>
+                        <td><b>0</b></td>
+                    </tr>
+                    <tr>
+                        <td>bestätigte Todesfälle:</td>
+                        <td><b>0</b></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <h5>Eingeleitete Gegenmaßnahmen im Spielcenter-Chemnitz</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item list-group-item-danger">22.03.2020 - Komplette Schließung der Anlage für unbestimmte Zeit auf Geheiß der Regierung</li>
+                <li class="list-group-item list-group-item-danger">22.05.2020 - Wiedereröffnung mit Umsetzung eines genehmigten Hygiene-Schutz-Konzeptes für unsere Gäste</li>
+                <li class="list-group-item list-group-item-danger">16.10.2020 - Anpassung und Erweiterung des Hygiene-Schutz-Konzeptes</li>
+                <li class="list-group-item list-group-item-danger">02.11.2020 - erneute Schließung auf Geheiß der Regierung</li>
+            </ul>
+        </div>
+    </div>
+    <div class="card-footer d-flex flex-column">
+        <a href="<?=base_url("hygienekonzept")?>" class="btn btn-danger mt-auto"><i class="fas fa-shield-virus"></i> aktuelles Hygiene-Konzept</a>
+    </div>
+</div>
+
 <img class="img-fluid w-100 mb-3" src="<?= base_url('assets/header/minigolf_dschungel.jpg') ?>" alt="Schwarzlicht / Adventure Minigolf Chemnitz Dschungel">
 <h2>Unsere Anlage</h2>
 <p>Ihr startet das Minigolf Abenteuer im Weltall. Seid darauf gefasst, das euch Aliens beobachten. Danach beamt ihr euch runter zur Erde. Genauer gesagt in einen Dschungel auf einer fernab gelegenen Insel. Es gleicht einem Jurassic Park, in dem überraschend noch wenige Arten von Dinosauriern überlebt haben.</p>
